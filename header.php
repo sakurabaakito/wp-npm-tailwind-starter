c96ebb46bbb53a1abbe15112a638d280dec588df
Sakuraba Akito <51189312+sakurabaakito@users.noreply.github.com>
2023-06-23 16:13:38 +0900

    Update header.php

--- header.php
+++ header.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <link rel="icon" href="<?= get_template_directory_uri() . "/img/favicon.ico" ?>">
+    <link rel="shortcut icon" href="<?= get_template_directory_uri() . "/img/favicon.svg" ?>">
 
     <?php wp_head(); ?>
 </head>
