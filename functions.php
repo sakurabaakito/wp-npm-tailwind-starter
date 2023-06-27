9d33ea667d4d36e27ed6c21e8f9922650ac5628d
Sakuraba Akito <51189312+sakurabaakito@users.noreply.github.com>
2023-06-27 17:23:26 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+// add_theme_support で追加される機能
+require_once locate_template('func/custom_theme_support.php', true);
+
 // CSS・JSの読み込み
 require_once locate_template('func/enqueue.php', true);
 
